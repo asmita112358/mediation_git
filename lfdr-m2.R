fb481d44deb35b64b57a446ec1997d8e0d56da2f
Asmita Roy <asmitaroy@Asmitas-MacBook-Air.local>
2022-10-31 15:31:17 -0500

    re-arranging the estimated pi

--- lfdr-m2.R
+++ lfdr-m2.R
@@ -1,41 +1,20 @@
 ##Implementation of 1d rejection region with a joint local fdr
+library(combinat)
 source("~/Documents/OneDrive - Texas A&M University/Documents/fair-ML/RCodes/mediation_git/EM2v2.R")
-##Generate data:
-J = 1000
-mu = 3
-theta = -3
-pi = c(0.7, 0.1, 0.1, 0.1)
-var1 = 1
-var2 = 2
 
-obj = generate(J, pi, var1, var2, mu, theta)
-samp = obj$sample
-gamma = obj$gamma
-tp = (gamma == 4)
-tn = (gamma != 4)
-##Running EM algorithm to estimate the parameters
-pi.init = c(0.8, 0.05, 0.05, 0.1)
-parm = maximization(samp, pi.init)$par
-
-##computing the joint local fdr
-
-##samp is a JX2 matrix with the first col as a, second col b
-##parm = the parameter in the order it's returned from the maximization function, barring the pi's.
-
-##Check this every time, or come up with a method to check which is closest
-
+##Function for computing cutoffs
 lfdr = function(samp, parm)
 {
   J = nrow(samp)
- a = samp[,1]
- b = samp[,2]
- pi = parm[1:4]
- mu = parm[5]
- theta = parm[6]
- var1 = parm[7]
- var2 = parm[8]
- a = samp[,1]
- b = samp[,2]
+  a = samp[,1]
+  b = samp[,2]
+  pi = parm[1:4]
+  mu = parm[5]
+  theta = parm[6]
+  var1 = parm[7]
+  var2 = parm[8]
+  a = samp[,1]
+  b = samp[,2]
   
   lfdr = vector()
   for(i in 1:J)
@@ -60,10 +39,87 @@ lfdr = function(samp, parm)
   return(list(cutoff = lfdrk, rej = reject, k = k))
 }
 
-obj = lfdr(samp, parm = parm)
-reject = obj$rej
-pow = sum(reject*tp)/sum(tp)
-mfdr = sum(tn*reject)/sum(reject)
 
-pow
-mfdr
+
+
+
+sim.fcn = function(J, pi, var1, var2, mu, theta, pi.init = c(0.8, 0.05, 0.05, 0.1))
+{
+  obj = generate(J, pi, var1, var2, mu, theta)
+  samp = obj$sample
+  gamma = obj$gamma
+  tp = (gamma == 4)
+  tn = (gamma != 4)
+ 
+  ##Running EM algorithm to estimate the parameters
+  
+  parm = maximization(samp, pi.init)$par
+  
+  
+  ##Checking which rearrangement of pi suits
+  
+  pi_est = parm[1:4]
+  
+  err = vector()
+  object = permn(pi_est)
+  for(i in 1:24)
+  {
+    err[i] = sum(abs((object[[i]]-pi)))
+  }
+  pi_new = object[[which.min(err)]]
+  
+  ##computing the joint local fdr
+  
+  ##samp is a JX2 matrix with the first col as a, second col b
+  ##Parm is the parameter vector
+  
+  parm = c(pi_new, parm[5:8])
+  
+  obj = lfdr(samp, parm = parm)
+  reject = obj$rej
+  pow = sum(reject*tp)/sum(tp)
+  mfdr = sum(tn*reject)/sum(reject)
+  
+  return(c(mfdr, pow))
+  
+  
+}
+
+
+##Dense null:
+##Generate data:
+J = 1000
+mu = 2
+theta = 2
+pi = c(0.7, 0.1, 0.1, 0.1)
+var1 = 0.5
+var2 = 0.7
+
+m = matrix(nrow = 20, ncol = 2)
+for(r in 1:20)
+{
+ m[r,] = sim.fcn(J, pi, var1, var2, mu, theta, pi.init = c(0.8, 0.05, 0.05, 0.1))
+ print(u) 
+}
+
+u = colMeans(m)
+v = apply(m,1, var)
+
+##Dense alternative
+J = 1000
+mu = 2
+theta = 2
+pi = c(0.3, 0.2, 0.2, 0.3)
+var1 = 0.5
+var2 = 0.7
+
+
+m = matrix(nrow = 20, ncol = 2)
+for(r in 1:20)
+{
+  m[r,] = sim.fcn(J, pi, var1, var2, mu, theta, pi.init = c(0.25, 0.25, 0.25, 0.25))
+  print(r) 
+}
+
+u = colMeans(m)
+v = apply(m,1, var)
