92db711277dd201ee8a03bf1e4487abf0945436e
Asmita Roy <asmitaroy@eduroamprvnat-172-16-63-131.near.illinois.edu>
2022-11-06 12:44:43 -0600

    Inserted the EM functions

--- functions_lfdr.R
+++ functions_lfdr.R
@@ -104,3 +104,108 @@ Q = function(alpha, beta,X, M, pi, sigma_a, sigma_b, mu, theta)
   
   return(t/rowSums(t))  ##Check this
 }
+
+
+LL.complete = function(sigma_a, sigma_b, alpha, beta, X, M, Qval, pi, mu, theta)
+{
+  m = nrow(M)
+  t = matrix(nrow = m, ncol = 4)
+  ##Insert code here for computing sigma1_i and sigma_2i from sigma_a and sigma_b
+  variances = var_fun(sigma_a, sigma_b, X, M)
+  var1 = variances[1]
+  var2 = variances[-1]
+  for(i in 1:m)
+  {
+    t[i,1] = pi[1]*dmvnorm(c(alpha[i], beta[i]), c(0,0), matrix(c(var1,0,0,var2[i]), nrow = 2))
+    t[i,2] = pi[2]*dmvnorm(c(alpha[i], beta[i]), c(mu,0), matrix(c(var1 + 1,0,0,var2[i]), nrow = 2))
+    t[i,3] = pi[3]*dmvnorm(c(alpha[i], beta[i]), c(0,theta), matrix(c(var1,0,0,var2[i] + 1), nrow = 2))
+    t[i,4] = pi[4]*dmvnorm(c(alpha[i], beta[i]), c(mu,theta), matrix(c(var1 + 1,0,0,var2[i] + 1), nrow = 2))
+    
+  }
+  return(sum(Qval*log(t)) + sum(Qval*log(pi)))
+}
+
+
+LL.data = function(sigma_a, sigma_b, alpha, beta, X, M, pi, mu, theta)
+{
+  m = nrow(M)
+  t = matrix(nrow = m, ncol = 4)
+  ##Insert code here for computing sigma1_i and sigma_2i from sigma_a and sigma_b
+  variances = var_fun(sigma_a, sigma_b, X, M)
+  var1 = variances[1]
+  var2 = variances[-1]
+  for(i in 1:m)
+  {
+    t[i,1] = pi[1]*dmvnorm(c(alpha[i], beta[i]), c(0,0), matrix(c(var1,0,0,var2[i]), nrow = 2))
+    t[i,2] = pi[2]*dmvnorm(c(alpha[i], beta[i]), c(mu,0), matrix(c(var1 + 1,0,0,var2[i]), nrow = 2))
+    t[i,3] = pi[3]*dmvnorm(c(alpha[i], beta[i]), c(0,theta), matrix(c(var1,0,0,var2[i] + 1), nrow = 2))
+    t[i,4] = pi[4]*dmvnorm(c(alpha[i], beta[i]), c(mu,theta), matrix(c(var1 + 1,0,0,var2[i] + 1), nrow = 2))
+    
+  }
+  return(sum(log(rowSums(pi*t))))
+}
+
+maximization = function(alpha, beta, X, Y, M, pi_start, maxiter = 1000) 
+{
+  m = nrow(M)
+  
+  ##Define starting values of parameters
+  
+  mu_start = mean(alpha)
+  theta_start = mean(beta)
+  sigma_a_start = sqrt(var(alpha)*mean(X^2))
+  sigma_b_start = sqrt(var(beta)/mean(X^2))
+  Qval = Q(alpha, beta, X, M, pi_start, sigma_a_start, sigma_b_start, mu_start, theta_start)
+  LL.start = LL.data(sigma_a_start, sigma_b_start, alpha, beta, X, M, pi_start, mu_start, theta_start)
+  
+  ##EM update
+  pi_new = colMeans(Qval)
+  mu_new = sum(Qval[,2]*alpha + Qval[,4]*alpha)/sum(Qval[,2] + Qval[,4])
+  theta_new = sum(Qval[,3]*beta + Qval[,4]*beta)/sum(Qval[,3] + Qval[,4])
+  sigma_a_new = optimize(LL.complete, interval = c(0,10) , sigma_b = 1,alpha = alpha, beta = beta,
+                         X = X, M = M, Qval = Qval, pi = pi_new, mu = mu_new, theta = theta_new, maximum = TRUE)$maximum
+  sigma_b_new = optimize(LL.complete, interval = c(0,10) , sigma_a = 1,alpha = alpha, beta = beta,
+                         X = X, M = M, Qval = Qval, pi = pi_new, mu = mu_new, theta = theta_new, maximum = TRUE)$maximum
+  
+  ##Computing the value of log-likelihood with the EM updates
+  LL.new = LL.data(sigma_a_new, sigma_b_new, alpha, beta, X, M, pi_new, mu_new, theta_new)
+  
+  ##Set a counter, start a while loop
+  counter = 1
+  LL.vec = vector()
+  LL.vec[counter] = LL.new
+  while(LL.new - LL.start > 1e-6)
+  {
+    counter = counter + 1
+    ##The new values from the last iteration becomes the starting values
+    LL.start = LL.new
+    LL.vec[counter] = LL.new
+    pi_start = pi_new
+    mu_start = mu_new
+    theta_start = theta_new
+    var1_start = var1_new
+    var2_start = var2_new
+    Qval = Q(alpha, beta, X, M, pi_start, sigma_a_start, sigma_b_start, mu_start, theta_start)
+    
+    
+    ##EM Update
+    pi_new = colMeans(Qval)
+    mu_new = sum(Qval[,2]*alpha + Qval[,4]*alpha)/sum(Qval[,2] + Qval[,4])
+    theta_new = sum(Qval[,3]*beta + Qval[,4]*beta)/sum(Qval[,3] + Qval[,4])
+    sigma_a_new = optimize(LL.complete, interval = c(0,10) , sigma_b = 1,alpha = alpha, beta = beta,
+                           X = X, M = M, Qval = Qval, pi = pi_new, mu = mu_new, theta = theta_new, maximum = TRUE)$maximum
+    sigma_b_new = optimize(LL.complete, interval = c(0,10) , sigma_a = 1,alpha = alpha, beta = beta,
+                           X = X, M = M, Qval = Qval, pi = pi_new, mu = mu_new, theta = theta_new, maximum = TRUE)$maximum
+    
+    
+    ##Increase counter and check the likelihood
+    
+    LL.new = LL.data(sigma_a_new, sigma_b_new, alpha, beta, X, M, pi_new, mu_new, theta_new)
+    #print(counter)
+    
+    ##If the while loop runs longer than maxiter then it's stopped forcibly.
+    if(counter > maxiter) {print(paste("LL=", LL.new));LL.new = LL.start}
+  }
+  return(list(likelihood = LL.vec, par = c(pi_new, mu_new, theta_new, var1_new, var2_new))) 
+  
+}  
